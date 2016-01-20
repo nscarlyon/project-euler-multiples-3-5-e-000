1161516c2a33e63b7d597d79158a927ebb773d8d
Natasha Carlyon <natasha.carlyon@gmail.com>
2016-01-20 04:05:12 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+
+        (1...limit).select { |multiple| multiple % 5 == 0 || multiple % 3 == 0 }
+end
+
+def sum_multiples(limit)
+
+        sum = 0
+        collect_multiples(limit).each { |multiple| sum += multiple}
+        return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+def initialize(limit)
+@limit = limit
+end
+
+        def collect_multiples
+
+        (1...@limit).select { |multiple| multiple % 5 == 0 || multiple % 3 == 0 }
+end
+
+def sum_multiples
+
+        sum = 0
+        collect_multiples.each { |multiple| sum += multiple}
+        return sum
+end
+
+end
